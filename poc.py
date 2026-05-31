bce309a91c057ca7fbe2e8bb770d0ca8c9036257
mckennahollinger <root@LAPTOP-ORTF9FMB.localdomain>
2026-05-31 16:21:50 -0700

    Refactored human translation target variables

--- poc.py
+++ poc.py
@@ -10,11 +10,11 @@ load_dotenv()
 
 # Load tokenizers for source and target texts
 source_tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
-target_tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
+human_target_tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
 
 # Initialize lists to iterate over each tokenized line of text
 source_tokens = []
-target_tokens = []
+human_target_tokens = []
 
 # Store corpora parent path into accessible variable
 parent_file_path = os.getenv("CORPORA_PARENT_PATH")
@@ -32,21 +32,21 @@ with open(source_file_path, "r") as source:
         source_tokens.append(source_tokenizer.tokenize(line))
 
 # Target corpora file path
-target_file_path = parent_file_path + ("/English/Decade_Of_Sheng_Min.txt")
+human_target_file_path = parent_file_path + ("/English/Decade_Of_Sheng_Min_Human.txt")
 
 # Tokenize each line in target text
-with open(target_file_path, "r") as target:
+with open(human_target_file_path, "r") as target:
     for line in target:
-        target_tokens.append(target_tokenizer.tokenize(line))
+        human_target_tokens.append(human_target_tokenizer.tokenize(line))
 
 # Initialize list to append SimAlign Itermax algorithm results to 
 alignments = []
 
 # Align source and target lines of text 
 for line in range(len(source_tokens)):
-    alignments.append(aligner.get_word_aligns(source_tokens[line], target_tokens[line]))
+    alignments.append(aligner.get_word_aligns(source_tokens[line], human_target_tokens[line]))
 
 # Print results of which words in each text have been aligned with each other
 for line, aligned in enumerate(alignments):
     for pair in aligned["itermax"]:
-        print(f"{source_tokens[line][pair[0]]} ({pair[0]}) === {target_tokens[line][pair[1]]} ({pair[1]})")
+        print(f"{source_tokens[line][pair[0]]} ({pair[0]}) === {human_target_tokens[line][pair[1]]} ({pair[1]})")
